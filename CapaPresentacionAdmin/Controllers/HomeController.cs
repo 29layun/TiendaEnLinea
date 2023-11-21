63072a0efb0d1aaa482a3494eff9f67b1484b73e
Miguel Alejandro Jiménez Toral <mjimenezt003@alumno.uaemex.mx>
2023-11-21 13:48:42 -0600

    Agregando view de usuarios

--- CapaPresentacionAdmin/Controllers/HomeController.cs
+++ CapaPresentacionAdmin/Controllers/HomeController.cs
@@ -12,24 +12,8 @@ namespace CapaPresentacionAdmin.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
+        public ActionResult Usuarios()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
-        public ActionResult Pruebas()
-        {
-
             return View();
         }
     }
