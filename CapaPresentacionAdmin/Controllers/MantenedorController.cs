7998baff35766ee95078214950546c1839744969
Miguel Alejandro Jiménez Toral <mjimenezt003@alumno.uaemex.mx>
2023-11-21 14:58:31 -0600

    Agregando views de mantenedor a la capa de presentacion de administrador

---
+++ CapaPresentacionAdmin/Controllers/MantenedorController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CapaPresentacionAdmin.Controllers
+{
+    public class MantenedorController : Controller
+    {
+        // GET: Mantenedor
+        public ActionResult Categorias()
+        {
+            return View();
+        }
+        public ActionResult Marcas()
+        {
+            return View();
+        }
+        public ActionResult Productos()
+        {
+            return View();
+        }
+    }
+}
