26ca1e9e987065ea85c4e87596985c784a03ee63
Miguel Alejandro Jiménez Toral <mjimenezt003@alumno.uaemex.mx>
2023-11-21 15:30:35 -0600

    Agregando referencias para la conexion con la base de datos

---
+++ CapaDatos/CD_Usuarios.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaEntidad;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CapaDatos
+{
+    public class CD_Usuarios
+    {
+        public List<USUARIOS> Listar()
+        {
+            List<USUARIOS> lista = new List<USUARIOS>();
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cn))
+                {
+                    string query = "select IdUsuario, Nombres, Apellidos, Correo, Clave, Reestablecer,Activo from USUARIOS";
+                }
+            }
+            catch
+            {
+
+            }
+            return lista;
+
+        }
+    }
+}
