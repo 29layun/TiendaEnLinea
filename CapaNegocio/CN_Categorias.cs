4ed0fde9830df9b07fdb32ba2bd1091b976c677c
Miguel Alejandro Jiménez Toral <mjimenezt003@alumno.uaemex.mx>
2023-12-15 13:11:46 -0600

    Agregando clase CN_Categorias a la capa de negocio, con el contenido de los metodos de insertar, editar y eliminar

---
+++ CapaNegocio/CN_Categorias.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CapaDatos;
+using CapaEntidad;
+namespace CapaNegocio
+{
+    public class CN_Categorias
+    {
+        private CD_Categorias objCapaDato = new CD_Categorias();
+
+        public List<CATEGORIAS> Listar()
+        {
+            return objCapaDato.Listar();
+        }
+
+        //Registrar
+        public int Registrar(CATEGORIAS obj, out string Mensaje)
+        {
+
+            Mensaje = string.Empty;
+
+
+            if (string.IsNullOrEmpty(obj.Descripcion) || string.IsNullOrWhiteSpace(obj.Descripcion))
+            {
+                Mensaje = "La descripcion de la categoria no puede ser vacio";
+            }if (string.IsNullOrEmpty(Mensaje))
+            {
+                return objCapaDato.Registrar(obj, out Mensaje);
+
+            }else {
+                return 0;
+            }
+        }
+
+        public bool Editar(CATEGORIAS obj, out string Mensaje)
+        {
+
+            Mensaje = string.Empty;
+
+
+            if (string.IsNullOrEmpty(obj.Descripcion) || string.IsNullOrWhiteSpace(obj.Descripcion))
+            {
+                Mensaje = "La descripcion de la categoria no puede ser vacio";
+            }
+
+
+            if (string.IsNullOrEmpty(Mensaje))
+            {
+
+                return objCapaDato.Editar(obj, out Mensaje);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+        public bool Eliminar(int id, out string Mensaje)
+        {
+            return objCapaDato.Eliminar(id, out Mensaje);
+        }
+    }
+}
