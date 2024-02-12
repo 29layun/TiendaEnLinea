d429abd84bdd7c29ef5eac2086a939d198706b1d
Miguel Alejandro Jiménez Toral <mjimenezt003@alumno.uaemex.mx>
2024-02-12 14:31:16 -0600

    Agregando objeto dashboard para ver los datos en cd_reportes en la capa de datos

---
+++ CapaDatos/CD_Reporte.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CapaEntidad;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CapaDatos
+{
+    public class CD_Reporte
+    {
+        public Dashboard VerDashboard()
+        {
+            Dashboard objeto = new Dashboard();
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cn))
+                {
+                    
+                    SqlCommand cmd = new SqlCommand("sp_ReporteDashboard", oconexion);
+
+                    cmd.CommandType = CommandType.Text;
+
+                    oconexion.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            objeto = new Dashboard()
+                            {
+                                TotalCliente = Convert.ToInt32(dr["TotalCliente"]),
+                                TotalVenta = Convert.ToInt32(dr["TotalVenta"]),
+                                TotalProducto = Convert.ToInt32(dr["TotalProducto"]),
+                            };
+                        }
+                    }
+                    oconexion.Close();
+                }
+            }
+            catch
+            {
+                objeto = new Dashboard();
+            }
+            return objeto;
+
+        }
+    }
+}
