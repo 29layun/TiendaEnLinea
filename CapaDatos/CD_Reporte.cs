f1d2d4770cb36d0bccde8574b05065a3b3195e79
Miguel Alejandro Jiménez Toral <mjimenezt003@alumno.uaemex.mx>
2024-02-14 13:56:31 -0600

    Agregando metodo de listar reporte de la tabla de dashboard

--- CapaDatos/CD_Reporte.cs
+++ CapaDatos/CD_Reporte.cs
@@ -14,6 +14,49 @@ namespace CapaDatos
 {
     public class CD_Reporte
     {
+        public List<Reporte> Ventas(string Fechainicio, string Fechafin, string Idtransaccion)
+        {
+            List<Reporte> lista = new List<Reporte>();
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cn))
+                {
+                    
+                    SqlCommand cmd = new SqlCommand("sp_ReporteVentas", oconexion);
+                    cmd.Parameters.AddWithValue("Fechainicio", Fechainicio);
+                    cmd.Parameters.AddWithValue("Fechafin", Fechafin);
+                    cmd.Parameters.AddWithValue("Idtransaccion", Idtransaccion);
+                    cmd.CommandType = CommandType.Text;
+
+                    oconexion.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            lista.Add(
+                                new Reporte()
+                                {
+                                    FechaVenta = dr["FechaVenta"].ToString(),
+                                    Cliente = dr["Cliente"].ToString(),
+                                    Producto = dr["Producto"].ToString(),
+                                    Precio = Convert.ToDecimal(dr["Precio"],new CultureInfo("es-MX")),
+                                    Cantidad = Convert.ToInt32(dr["Cantidad"].ToString()),
+                                    Total = Convert.ToDecimal(dr["Total"],new CultureInfo("es-MX")),
+                                    IdTransaccion = dr["IdTransaccion"].ToString()
+                                });
+                        }
+                    }
+                    oconexion.Close();
+                }
+            }
+            catch
+            {
+                lista = new List<Reporte>();
+            }
+            return lista;
+
+        }
         public DashBoard VerDashBoard()
         {
 
